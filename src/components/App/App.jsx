e1e24f223b85be427570498276b5957ad920cc63
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-08-08 14:25:39 +0300

    Создать окружение

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import './components/App/App.css';
+
+function App() {
+    return (
+            <div className="App">
+                <header className="App-header">
+                </header>
+            </div>
+    );
+}
+
+export default App;
